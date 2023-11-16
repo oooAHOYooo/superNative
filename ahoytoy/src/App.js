a5147dce134f86b7aac1892bc32bf5b0630f7099
agSuperCoder123 <alex@ahoy.ooo>
2023-11-16 08:02:12 -0500

    think I got the thing fixing

--- ahoytoy/src/App.js
+++ ahoytoy/src/App.js
@@ -1,46 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import NowPlaying from './components/NowPlaying';
-import Radio from './components/Radio';
-import Artists from './components/Artists';
+
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/now-playing">Now Playing</Link>
-            </li>
-            <li>
-              <Link to="/radio">Radio</Link>
-            </li>
-            <li>
-              <Link to="/artists">Artists</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/now-playing">
-            <NowPlaying />
-          </Route>
-          <Route path="/radio">
-            <Radio />
-          </Route>
-          <Route path="/artists">
-            <Artists />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+<div className="hi">Hiii</div>
   );
 }
 
