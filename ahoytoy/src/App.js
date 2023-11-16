f79f50b095a4c94ea9e197db9027cd93856ca22d
agSuperCoder123 <alex@ahoy.ooo>
2023-11-16 06:38:14 -0500

    added the first basic now playing page and attempted to link it with the router

--- ahoytoy/src/App.js
+++ ahoytoy/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import NowPlaying from './components/NowPlaying';
+import Radio from './components/Radio';
+import Artists from './components/Artists';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/now-playing">Now Playing</Link>
+            </li>
+            <li>
+              <Link to="/radio">Radio</Link>
+            </li>
+            <li>
+              <Link to="/artists">Artists</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/now-playing">
+            <NowPlaying />
+          </Route>
+          <Route path="/radio">
+            <Radio />
+          </Route>
+          <Route path="/artists">
+            <Artists />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home Page</h2>; // Simple home component for demonstration
 }
 
 export default App;
